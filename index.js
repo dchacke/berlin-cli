eb1fc9f0da1592d876e2a18a18ab392c397f52cb
Dennis C Hackethal <dennis.hackethal@gmail.com>
2020-06-17 15:01:08 -0700

    Implement simple CLI
    It can take a source-file path and target-file path, transpiles the source and spits it into the target file.

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+let fs = require("fs");
+let resolve = require("path").resolve
+let { transpile } = require("../lang/transpiler");
+
+let [sourcePath, targetPath] = process.argv.slice(2);
+let sourceCode = fs.readFileSync(sourcePath, { encoding: "utf8"});
+let transpilation = transpile(sourceCode);
+
+fs.writeFile(targetPath, transpilation, err => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Transpilation successful.\nStored in ${resolve(targetPath)}`);
+});
