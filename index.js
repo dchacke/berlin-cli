2a134c03bf9b2ed213283683f8498787537ddc3c
Dennis C Hackethal <dennis.hackethal@gmail.com>
2020-06-21 23:48:23 -0700

    Implement simple repl

--- index.js
+++ index.js
@@ -2,15 +2,38 @@
 let fs = require("fs");
 let resolve = require("path").resolve
 let { transpile } = require("berlin-lang/transpiler");
+let core = require("berlin-lang/core");
+let vm = require('vm');
+let prompt = require('prompt-sync')({ sigint: true });
 
-let [sourcePath, targetPath] = process.argv.slice(2);
-let sourceCode = fs.readFileSync(sourcePath, { encoding: "utf8"});
-let transpilation = transpile(sourceCode);
+// The command was simply "berlin". We want to run
+// the repl.
+if (process.argv.length <= 2) {
+  const context = core;
+  vm.createContext(context);
 
-fs.writeFile(targetPath, transpilation, err => {
-  if (err) {
-    throw err;
+  while (true) {
+    let input = prompt("berlin > ");
+
+    if (input === "exit") {
+      break;
+    }
+
+    let result = vm.runInContext(transpile(input), context);
+    console.log(result);
   }
+// The command was invoked with a source and target path.
+// We want to transpile.
+} else {
+  let [sourcePath, targetPath] = process.argv.slice(2);
+  let sourceCode = fs.readFileSync(sourcePath, { encoding: "utf8"});
+  let transpilation = transpile(sourceCode);
 
-  console.log(`Transpilation successfully written to ${resolve(targetPath)}`);
-});
+  fs.writeFile(targetPath, transpilation, err => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`Transpilation successfully written to ${resolve(targetPath)}`);
+  });
+}
